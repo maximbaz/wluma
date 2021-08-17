f45cdbd761ac052b2a828a248cdfe642e47251b6
Maxim Baz <git@maximbaz.com>
2021-08-17 17:54:52 +0200

    Allow skipping same values in hour_to_lux

--- src/als/time.rs
+++ src/als/time.rs
@@ -9,10 +9,18 @@ pub struct Als {
 impl Als {
     pub fn new(hour_to_lux: &HashMap<String, u32>) -> Result<Self, Box<dyn Error>> {
         Ok(Self {
-            hour_to_lux: hour_to_lux
-                .iter()
-                .map(|(key, &val)| key.parse().map(|k| (k, val)))
-                .collect::<Result<_, _>>()?,
+            hour_to_lux: (0..24)
+                .into_iter()
+                .fold(Vec::<(u32, u32)>::new(), |mut acc, hour| {
+                    let lux = hour_to_lux
+                        .get(&hour.to_string())
+                        .map(|&v| v)
+                        .unwrap_or(acc.last().map(|&v| v.1).unwrap_or(0));
+                    acc.push((hour, lux));
+                    acc
+                })
+                .into_iter()
+                .collect(),
         })
     }
 }
