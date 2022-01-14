77cc3d2963720ffca983d931e6372b010deca8a4
Cyril Levis <git@levis.name>
2022-01-14 22:03:53 +0100

    chore: remove debug, calc pixels earlier

--- src/als/webcam.rs
+++ src/als/webcam.rs
@@ -51,14 +51,14 @@ impl Webcam {
     }
 
     fn frame(&mut self) -> Result<(Vec<u8>, usize), Box<dyn Error>> {
-        let (device, width, height) = Self::setup(self.video)?;
+        let (device, pixels) = Self::setup(self.video)?;
         let mut stream = Stream::new(&device, Type::VideoCapture)?;
         let (rgbs, _) = stream.next()?;
 
-        Ok((rgbs.to_vec(), width * height))
+        Ok((rgbs.to_vec(), pixels))
     }
 
-    fn setup(video: usize) -> Result<(Device, usize, usize), Box<dyn Error>> {
+    fn setup(video: usize) -> Result<(Device, usize), Box<dyn Error>> {
         let device = Device::new(video)?;
         let mut format = device.format()?;
         format.fourcc = FourCC::new(b"RGB3");
@@ -73,14 +73,13 @@ impl Webcam {
                     .collect_vec()
             })
             .min_by(|&(w1, h1), &(w2, h2)| h1.cmp(&h2).then(w1.cmp(&w2)))
-            .ok_or("Unable to minimum resolution")?;
+            .ok_or("Unable to find minimum resolution")?;
 
-        log::debug!("ALS (webcam) resolution used: {:?}x{:?}", width, height);
         format.height = height;
         format.width = width;
         device.set_format(&format)?;
 
-        Ok((device, width as usize, height as usize))
+        Ok((device, width as usize * height as usize))
     }
 }
 
