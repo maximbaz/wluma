b0df54ff6ee026c34fa6aae8ae256d061a1b1e9b
Maxim Baz <git@maximbaz.com>
2022-01-14 14:26:22 +0100

    park main thread
    it's impossible to really join multiple forever-running threads anyway

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn main() {
     let config_outputs = config.output;
     let config_als = config.als;
 
-    let (als_txs, threads): (_, Vec<_>) = config_outputs
+    let als_txs = config_outputs
         .into_iter()
         .map(move |output| {
             let config = match config::Config::load() {
@@ -48,110 +48,95 @@ fn main() {
             let output_name_t1 = output_name.clone();
             let output_name_t2 = output_name.clone();
 
-            (
-                als_tx,
-                vec![
-                    std::thread::Builder::new()
-                        .name(format!("backlight-{}", output_name))
-                        .spawn(move || {
-                            let brightness = match output {
-                                config::Output::Backlight(cfg) => {
-                                    brightness::Backlight::new(&cfg.path)
-                                        .map(|b| Box::new(b) as Box<dyn brightness::Brightness>)
-                                }
-                                config::Output::DdcUtil(cfg) => brightness::DdcUtil::new(&cfg.name)
-                                    .map(|b| Box::new(b) as Box<dyn brightness::Brightness>),
-                            };
-
-                            match brightness {
-                                Ok(b) => {
-                                    brightness::Controller::new(b, user_tx, prediction_rx).run();
-                                }
-                                Err(err) => log::warn!(
-                                    "Skipping output '{}' as it might be disconnected: {}",
-                                    output_name_t1,
-                                    err
-                                ),
-                            };
-                        })
-                        .expect("Unable to start backlight thread"),
-                    std::thread::Builder::new()
-                        .name(format!("predictor-{}", output_name))
-                        .spawn(move || {
-                            let frame_processor: Box<dyn frame::processor::Processor> =
-                                match config.frame.processor {
-                                    config::Processor::Vulkan => Box::new(
-                                        frame::processor::vulkan::Processor::new()
-                                            .expect("Unable to initialize Vulkan"),
-                                    ),
-                                };
-
-                            let frame_capturer: Box<dyn frame::capturer::Capturer> =
-                                match config.frame.capturer {
-                                    config::Capturer::Wlroots => Box::new(
-                                        frame::capturer::wlroots::Capturer::new(frame_processor),
-                                    ),
-                                    config::Capturer::None => {
-                                        Box::new(frame::capturer::none::Capturer::default())
-                                    }
-                                };
-
-                            let controller = predictor::Controller::new(
-                                prediction_tx,
-                                user_rx,
-                                als_rx,
-                                true,
-                                &output_name_t2,
-                            );
-                            frame_capturer.run(&output_name_t2, controller)
-                        })
-                        .expect("Unable to start predictor thread"),
-                ],
-            )
-        })
-        .unzip();
-
-    let threads = threads
-        .into_iter()
-        .flatten()
-        .chain(std::iter::once(
             std::thread::Builder::new()
-                .name("als".to_string())
+                .name(format!("backlight-{}", output_name))
                 .spawn(move || {
-                    let als: Box<dyn als::Als> = match config_als {
-                        config::Als::Iio {
-                            path, thresholds, ..
-                        } => Box::new(
-                            als::iio::Als::new(&path, thresholds)
-                                .expect("Unable to initialize ALS IIO sensor"),
-                        ),
-                        config::Als::Time { thresholds, .. } => {
-                            Box::new(als::time::Als::new(thresholds))
-                        }
-                        config::Als::Webcam {
-                            video, thresholds, ..
-                        } => Box::new({
-                            let (webcam_tx, webcam_rx) = mpsc::channel();
-                            std::thread::Builder::new()
-                                .name("als-webcam".to_string())
-                                .spawn(move || {
-                                    als::webcam::Webcam::new(webcam_tx, video).run();
-                                })
-                                .expect("Unable to start webcam als");
-                            als::webcam::Als::new(webcam_rx, thresholds)
-                        }),
-                        config::Als::None => Box::new(als::none::Als::default()),
+                    let brightness = match output {
+                        config::Output::Backlight(cfg) => brightness::Backlight::new(&cfg.path)
+                            .map(|b| Box::new(b) as Box<dyn brightness::Brightness>),
+                        config::Output::DdcUtil(cfg) => brightness::DdcUtil::new(&cfg.name)
+                            .map(|b| Box::new(b) as Box<dyn brightness::Brightness>),
                     };
 
-                    als::controller::Controller::new(als, als_txs).run();
+                    match brightness {
+                        Ok(b) => {
+                            brightness::Controller::new(b, user_tx, prediction_rx).run();
+                        }
+                        Err(err) => log::warn!(
+                            "Skipping output '{}' as it might be disconnected: {}",
+                            output_name_t1,
+                            err
+                        ),
+                    };
                 })
-                .expect("Unable to start als"),
-        ))
+                .unwrap_or_else(|_| panic!("Unable to start thread: backlight-{}", output_name));
+
+            std::thread::Builder::new()
+                .name(format!("predictor-{}", output_name))
+                .spawn(move || {
+                    let frame_processor: Box<dyn frame::processor::Processor> =
+                        match config.frame.processor {
+                            config::Processor::Vulkan => Box::new(
+                                frame::processor::vulkan::Processor::new()
+                                    .expect("Unable to initialize Vulkan"),
+                            ),
+                        };
+
+                    let frame_capturer: Box<dyn frame::capturer::Capturer> =
+                        match config.frame.capturer {
+                            config::Capturer::Wlroots => {
+                                Box::new(frame::capturer::wlroots::Capturer::new(frame_processor))
+                            }
+                            config::Capturer::None => {
+                                Box::new(frame::capturer::none::Capturer::default())
+                            }
+                        };
+
+                    let controller = predictor::Controller::new(
+                        prediction_tx,
+                        user_rx,
+                        als_rx,
+                        true,
+                        &output_name_t2,
+                    );
+                    frame_capturer.run(&output_name_t2, controller)
+                })
+                .unwrap_or_else(|_| panic!("Unable to start thread: predictor-{}", output_name));
+
+            als_tx
+        })
         .collect_vec();
 
-    println!("Continue adjusting brightness and wluma will learn your preference over time.");
+    std::thread::Builder::new()
+        .name("als".to_string())
+        .spawn(move || {
+            let als: Box<dyn als::Als> = match config_als {
+                config::Als::Iio {
+                    path, thresholds, ..
+                } => Box::new(
+                    als::iio::Als::new(&path, thresholds)
+                        .expect("Unable to initialize ALS IIO sensor"),
+                ),
+                config::Als::Time { thresholds, .. } => Box::new(als::time::Als::new(thresholds)),
+                config::Als::Webcam {
+                    video, thresholds, ..
+                } => Box::new({
+                    let (webcam_tx, webcam_rx) = mpsc::channel();
+                    std::thread::Builder::new()
+                        .name("als-webcam".to_string())
+                        .spawn(move || {
+                            als::webcam::Webcam::new(webcam_tx, video).run();
+                        })
+                        .expect("Unable to start thread: als-webcam");
+                    als::webcam::Als::new(webcam_rx, thresholds)
+                }),
+                config::Als::None => Box::new(als::none::Als::default()),
+            };
 
-    threads
-        .into_iter()
-        .for_each(|t| t.join().expect("Error running thread"));
+            als::controller::Controller::new(als, als_txs).run();
+        })
+        .expect("Unable to start thread: als");
+
+    println!("Continue adjusting brightness and wluma will learn your preference over time.");
+    std::thread::park();
 }
