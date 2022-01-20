bb5714ccf856259644a3df8316655723c1ca9ea5
Cyril Levis <git@levis.name>
2022-01-20 17:33:29 +0100

    fix: use inotify to detect backlight change
    make cpu cooler

--- src/brightness/backlight.rs
+++ src/brightness/backlight.rs
@@ -1,14 +1,18 @@
 use crate::device_file::{read, write};
+use inotify::{Inotify, WatchMask};
 use std::cell::RefCell;
 use std::error::Error;
 use std::fs;
 use std::fs::{File, OpenOptions};
+use std::io::ErrorKind;
 use std::path::Path;
 
 pub struct Backlight {
     file: RefCell<File>,
     min_brightness: u64,
     max_brightness: u64,
+    inotify: Inotify,
+    current: Option<u64>,
 }
 
 impl Backlight {
@@ -22,22 +26,55 @@ impl Backlight {
             .trim()
             .parse()?;
 
+        let brightness_path = Path::new(path).join("brightness");
+
+        let mut inotify = Inotify::init()?;
+        inotify.add_watch(brightness_path, WatchMask::MODIFY)?;
+
+        let brightness_hw_changed_path = Path::new(path).join("brightness_hw_changed");
+        if Path::new(&brightness_hw_changed_path).exists() {
+            inotify.add_watch(brightness_hw_changed_path, WatchMask::MODIFY)?;
+        }
+
+        let file = RefCell::new(file);
+
         Ok(Self {
-            file: RefCell::new(file),
+            file,
             min_brightness,
             max_brightness,
+            inotify,
+            current: None,
         })
     }
 }
 
 impl super::Brightness for Backlight {
-    fn get(&self) -> Result<u64, Box<dyn Error>> {
-        Ok(read(&mut self.file.borrow_mut())? as u64)
+    fn get(&mut self) -> Result<u64, Box<dyn Error>> {
+        let mut buffer = [0u8; 1024];
+        return match (self.inotify.read_events(&mut buffer), self.current) {
+            (_, None) => {
+                let value = read(&mut self.file.borrow_mut())? as u64;
+                self.current = Some(value);
+                Ok(value)
+            }
+            (Ok(mut event), Some(value)) => {
+                if event.next().is_some() {
+                    let value = read(&mut self.file.borrow_mut())? as u64;
+                    self.current = Some(value);
+                }
+                Ok(value)
+            }
+            (Err(error), Some(value)) if error.kind() == ErrorKind::WouldBlock => Ok(value),
+            (_, Some(value)) => Ok(value),
+        };
     }
 
-    fn set(&self, value: u64) -> Result<u64, Box<dyn Error>> {
-        let value = value.max(self.min_brightness).min(self.max_brightness);
+    fn set(&mut self, value: u64) -> Result<u64, Box<dyn Error>> {
+        let mut buffer = [0u8; 1024];
+        let value = value.max(self.min_brightness).min(self.max_brightness) as u64;
+        self.current = Some(value);
         write(&mut self.file.borrow_mut(), value as f64)?;
+        self.inotify.read_events(&mut buffer)?;
         Ok(value)
     }
 }
