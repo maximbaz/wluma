7ec511447f860c407dc4005ae3cf794e2767b62c
Maxim Baz <git@maximbaz.com>
2024-07-18 15:08:44 +0200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, naersk }:
+    let systems = [ "x86_64-linux" "aarch64-linux" ];
+    in flake-utils.lib.eachSystem systems (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+        libs = with pkgs; [
+          udev
+          v4l-utils
+          vulkan-loader
+          dbus
+        ];
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage {
+          src = ./.;
+          meta.mainProgram = "wluma";
+          nativeBuildInputs = with pkgs; [
+            makeWrapper
+            pkg-config
+            rustPlatform.bindgenHook
+            marked-man
+          ];
+          buildInputs = libs;
+        };
+        devShell = with pkgs; mkShell {
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy pkg-config ] ++ libs;
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          LIBCLANG_PATH = "${llvmPackages_12.libclang.lib}/lib";
+          BINDGEN_EXTRA_CLANG_ARGS = [
+            ''-I"${llvmPackages_12.libclang.lib}/lib/clang/${llvmPackages_12.libclang.version}/include"''
+          ] ++ (builtins.map (a: ''-I"${a}/include"'') [
+            glibc.dev
+            libv4l.dev
+          ]);
+        };
+      }
+    );
+}
