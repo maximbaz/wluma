08b48960cbb8325a8546d51aadc3ed5461747e74
Rishik Yalamanchili <73787402+Rishik-Y@users.noreply.github.com>
2024-09-18 01:39:54 +0530

    Update ash & some other deps (#118)

--- src/frame/vulkan.rs
+++ src/frame/vulkan.rs
@@ -32,7 +32,7 @@ pub struct Vulkan {
 impl Vulkan {
     pub fn new() -> Result<Self, Box<dyn Error>> {
         let app_name = CString::new("wluma")?;
-        let app_info = vk::ApplicationInfo::builder()
+        let app_info = vk::ApplicationInfo::default()
             .application_name(&app_name)
             .application_version(WLUMA_VERSION)
             .engine_name(&app_name)
@@ -40,45 +40,44 @@ impl Vulkan {
             .api_version(VULKAN_VERSION);
 
         let instance_extensions = &[
-            vk::KhrExternalMemoryCapabilitiesFn::name().as_ptr(),
-            vk::KhrGetPhysicalDeviceProperties2Fn::name().as_ptr(),
+            vk::KHR_EXTERNAL_MEMORY_CAPABILITIES_NAME.as_ptr(),
+            vk::KHR_GET_PHYSICAL_DEVICE_PROPERTIES2_NAME.as_ptr(),
         ];
 
         let entry = Entry::linked();
 
-        let create_info = vk::InstanceCreateInfo::builder()
+        let create_info = vk::InstanceCreateInfo::default()
             .application_info(&app_info)
             .enabled_extension_names(instance_extensions);
 
         let instance = unsafe {
             entry
                 .create_instance(&create_info, None)
-                .map_err(|e| Box::new(e) as Box<dyn Error>)?
+                .map_err(anyhow::Error::msg)?
         };
 
         let physical_devices = unsafe {
             instance
                 .enumerate_physical_devices()
-                .map_err(|e| Box::new(e) as Box<dyn Error>)?
+                .map_err(anyhow::Error::msg)?
         };
         let physical_device = *physical_devices
             .first()
             .ok_or("Unable to find a physical device")?;
 
         let queue_family_index = 0;
-        let queue_info = &[vk::DeviceQueueCreateInfo::builder()
+        let queue_info = &[vk::DeviceQueueCreateInfo::default()
             .queue_family_index(queue_family_index)
-            .queue_priorities(&[1.0])
-            .build()];
+            .queue_priorities(&[1.0])];
 
         let device_extensions = &[
-            vk::KhrExternalMemoryFn::name().as_ptr(),
-            vk::KhrExternalMemoryFdFn::name().as_ptr(),
-            vk::ExtExternalMemoryDmaBufFn::name().as_ptr(),
+            vk::KHR_EXTERNAL_MEMORY_CAPABILITIES_NAME.as_ptr(),
+            vk::KHR_EXTERNAL_MEMORY_FD_NAME.as_ptr(),
+            vk::EXT_EXTERNAL_MEMORY_DMA_BUF_NAME.as_ptr(),
         ];
-        let features = vk::PhysicalDeviceFeatures::builder();
+        let features = vk::PhysicalDeviceFeatures::default();
 
-        let device_create_info = vk::DeviceCreateInfo::builder()
+        let device_create_info = vk::DeviceCreateInfo::default()
             .queue_create_infos(queue_info)
             .enabled_extension_names(device_extensions)
             .enabled_features(&features);
@@ -86,18 +85,22 @@ impl Vulkan {
         let device = unsafe {
             instance
                 .create_device(physical_device, &device_create_info, None)
-                .map_err(|e| Box::new(e) as Box<dyn Error>)?
+                .map_err(anyhow::Error::msg)?
         };
 
         let queue = unsafe { device.get_device_queue(queue_family_index, 0) };
 
-        let pool_create_info = vk::CommandPoolCreateInfo::builder()
+        let pool_create_info = vk::CommandPoolCreateInfo::default()
             .flags(vk::CommandPoolCreateFlags::RESET_COMMAND_BUFFER)
             .queue_family_index(queue_family_index);
 
-        let command_pool = unsafe { device.create_command_pool(&pool_create_info, None)? };
+        let command_pool = unsafe {
+            device
+                .create_command_pool(&pool_create_info, None)
+                .map_err(anyhow::Error::msg)?
+        };
 
-        let command_buffer_allocate_info = vk::CommandBufferAllocateInfo::builder()
+        let command_buffer_allocate_info = vk::CommandBufferAllocateInfo::default()
             .command_buffer_count(1)
             .command_pool(command_pool)
             .level(vk::CommandBufferLevel::PRIMARY);
@@ -105,10 +108,10 @@ impl Vulkan {
         let command_buffers = unsafe {
             device
                 .allocate_command_buffers(&command_buffer_allocate_info)
-                .map_err(|e| Box::new(e) as Box<dyn Error>)?
+                .map_err(anyhow::Error::msg)?
         };
 
-        let buffer_info = vk::BufferCreateInfo::builder()
+        let buffer_info = vk::BufferCreateInfo::default()
             .size(BUFFER_PIXELS)
             .usage(vk::BufferUsageFlags::TRANSFER_DST)
             .sharing_mode(vk::SharingMode::EXCLUSIVE);
@@ -116,7 +119,7 @@ impl Vulkan {
         let buffer = unsafe {
             device
                 .create_buffer(&buffer_info, None)
-                .map_err(|e| Box::new(e) as Box<dyn Error>)?
+                .map_err(anyhow::Error::msg)?
         };
 
         let buffer_memory_req = unsafe { device.get_buffer_memory_requirements(buffer) };
@@ -140,20 +143,20 @@ impl Vulkan {
         let buffer_memory = unsafe {
             device
                 .allocate_memory(&allocate_info, None)
-                .map_err(|e| Box::new(e) as Box<dyn Error>)?
+                .map_err(anyhow::Error::msg)?
         };
 
         unsafe {
             device
                 .bind_buffer_memory(buffer, buffer_memory, 0)
-                .map_err(|e| Box::new(e) as Box<dyn Error>)?
+                .map_err(anyhow::Error::msg)?
         };
 
-        let fence_create_info = vk::FenceCreateInfo::builder();
+        let fence_create_info = vk::FenceCreateInfo::default();
         let fence = unsafe {
             device
                 .create_fence(&fence_create_info, None)
-                .map_err(|e| Box::new(e) as Box<dyn Error>)?
+                .map_err(anyhow::Error::msg)?
         };
 
         Ok(Self {
@@ -205,12 +208,15 @@ impl Vulkan {
 
         let pixels = mip_width as usize * mip_height as usize;
         let rgbas = unsafe {
-            let buffer_pointer = self.device.map_memory(
-                self.buffer_memory,
-                0,
-                vk::WHOLE_SIZE,
-                vk::MemoryMapFlags::empty(),
-            )?;
+            let buffer_pointer = self
+                .device
+                .map_memory(
+                    self.buffer_memory,
+                    0,
+                    vk::WHOLE_SIZE,
+                    vk::MemoryMapFlags::empty(),
+                )
+                .map_err(anyhow::Error::msg)?;
             std::slice::from_raw_parts(buffer_pointer as *mut u8, pixels * 4)
         };
 
@@ -220,7 +226,7 @@ impl Vulkan {
             self.device.unmap_memory(self.buffer_memory);
             self.device
                 .reset_fences(&[self.fence])
-                .map_err(|e| Box::new(e) as Box<dyn Error>)?;
+                .map_err(anyhow::Error::msg)?;
             self.device.destroy_image(frame_image, None);
             self.device.free_memory(frame_image_memory, None);
         }
@@ -231,7 +237,7 @@ impl Vulkan {
     fn init_image(&self, frame: &Object) -> Result<(), Box<dyn Error>> {
         let (width, height, mip_levels) = image_dimensions(frame);
 
-        let image_create_info = vk::ImageCreateInfo::builder()
+        let image_create_info = vk::ImageCreateInfo::default()
             .image_type(vk::ImageType::TYPE_2D)
             .format(vk::Format::B8G8R8A8_UNORM)
             .extent(vk::Extent3D {
@@ -247,18 +253,28 @@ impl Vulkan {
             .usage(vk::ImageUsageFlags::TRANSFER_DST | vk::ImageUsageFlags::TRANSFER_SRC)
             .sharing_mode(vk::SharingMode::EXCLUSIVE);
 
-        let image = unsafe { self.device.create_image(&image_create_info, None)? };
+        let image = unsafe {
+            self.device
+                .create_image(&image_create_info, None)
+                .map_err(anyhow::Error::msg)?
+        };
         let image_memory_req = unsafe { self.device.get_image_memory_requirements(image) };
 
-        let image_allocate_info = vk::MemoryAllocateInfo::builder()
+        let image_allocate_info = vk::MemoryAllocateInfo::default()
             .allocation_size(image_memory_req.size)
             .memory_type_index(0);
 
-        let image_memory = unsafe { self.device.allocate_memory(&image_allocate_info, None)? };
+        let image_memory = unsafe {
+            self.device
+                .allocate_memory(&image_allocate_info, None)
+                .map_err(anyhow::Error::msg)?
+        };
 
         unsafe {
-            self.device.bind_image_memory(image, image_memory, 0)?;
-        }
+            self.device
+                .bind_image_memory(image, image_memory, 0)
+                .map_err(anyhow::Error::msg)?
+        };
 
         self.image.borrow_mut().replace(image);
         self.image_memory.borrow_mut().replace(image_memory);
@@ -272,10 +288,12 @@ impl Vulkan {
         &self,
         frame: &Object,
     ) -> Result<(vk::Image, vk::DeviceMemory), Box<dyn Error>> {
-        let mut frame_image_memory_info = vk::ExternalMemoryImageCreateInfo::builder()
+        // External memory info
+        let mut frame_image_memory_info = vk::ExternalMemoryImageCreateInfo::default()
             .handle_types(vk::ExternalMemoryHandleTypeFlags::DMA_BUF_EXT);
 
-        let frame_image_create_info = vk::ImageCreateInfo::builder()
+        // Image create info
+        let frame_image_create_info = vk::ImageCreateInfo::default()
             .push_next(&mut frame_image_memory_info)
             .image_type(vk::ImageType::TYPE_2D)
             .format(vk::Format::R8G8B8A8_UNORM)
@@ -292,16 +310,22 @@ impl Vulkan {
             .usage(vk::ImageUsageFlags::TRANSFER_SRC)
             .sharing_mode(vk::SharingMode::EXCLUSIVE);
 
-        let frame_image = unsafe { self.device.create_image(&frame_image_create_info, None)? };
+        let frame_image = unsafe {
+            self.device
+                .create_image(&frame_image_create_info, None)
+                .map_err(anyhow::Error::msg)?
+        };
 
+        // Memory requirements info
         let frame_image_memory_req_info =
-            vk::ImageMemoryRequirementsInfo2::builder().image(frame_image);
+            vk::ImageMemoryRequirementsInfo2::default().image(frame_image);
 
-        // Prepare the structures to get memory requirements into, then get the requirements
+        // Prepare the structures to get memory requirements into, then get the memory requirements
         let mut frame_image_mem_dedicated_req = vk::MemoryDedicatedRequirements::default();
-        let mut frame_image_mem_req = vk::MemoryRequirements2::builder()
-            .push_next(&mut frame_image_mem_dedicated_req)
-            .build();
+
+        let mut frame_image_mem_req =
+            vk::MemoryRequirements2::default().push_next(&mut frame_image_mem_dedicated_req);
+
         unsafe {
             self.device.get_image_memory_requirements2(
                 &frame_image_memory_req_info,
@@ -312,21 +336,26 @@ impl Vulkan {
         // Bit i in memory_type_bits is set if the ith memory type in the
         // VkPhysicalDeviceMemoryProperties structure is supported for the image memory.
         // We just use the first type supported (from least significant bit's side)
+
+        // Find suitable memory type index
         let memory_type_index = frame_image_mem_req
             .memory_requirements
             .memory_type_bits
             .trailing_zeros();
 
+        // Import memory app_info
         // Construct the memory alloctation info according to the requirements
         // If the image needs dedicated memory, add MemoryDedicatedAllocateInfo to the info chain
-        let mut frame_import_memory_info = vk::ImportMemoryFdInfoKHR::builder()
+        let mut frame_import_memory_info = vk::ImportMemoryFdInfoKHR::default()
             .handle_type(vk::ExternalMemoryHandleTypeFlags::DMA_BUF_EXT)
             .fd(frame.fds[0]);
 
+        // dedicated allocation info
         let mut frame_image_memory_dedicated_info =
-            vk::MemoryDedicatedAllocateInfo::builder().image(frame_image);
+            vk::MemoryDedicatedAllocateInfo::default().image(frame_image);
 
-        let mut frame_image_allocate_info = vk::MemoryAllocateInfo::builder()
+        // Memory allocate info
+        let mut frame_image_allocate_info = vk::MemoryAllocateInfo::default()
             .push_next(&mut frame_import_memory_info)
             .allocation_size(frame_image_mem_req.memory_requirements.size)
             .memory_type_index(memory_type_index);
@@ -336,16 +365,18 @@ impl Vulkan {
                 frame_image_allocate_info.push_next(&mut frame_image_memory_dedicated_info);
         }
 
-        // Allocate the memory and bind it to the image
+        // Allocate memory and bind it to the image
         let frame_image_memory = unsafe {
             self.device
-                .allocate_memory(&frame_image_allocate_info, None)?
+                .allocate_memory(&frame_image_allocate_info, None)
+                .map_err(anyhow::Error::msg)?
         };
 
         unsafe {
             self.device
-                .bind_image_memory(frame_image, frame_image_memory, 0)?;
-        }
+                .bind_image_memory(frame_image, frame_image_memory, 0)
+                .map_err(anyhow::Error::msg)?;
+        };
 
         Ok((frame_image, frame_image_memory))
     }
@@ -362,17 +393,16 @@ impl Vulkan {
         dst_access_mask: vk::AccessFlags,
         src_stage_mask: vk::PipelineStageFlags,
     ) {
-        let image_barrier = vk::ImageMemoryBarrier::builder()
+        let image_barrier = vk::ImageMemoryBarrier::default()
             .old_layout(old_layout)
             .new_layout(new_layout)
             .image(*image)
             .subresource_range(
-                vk::ImageSubresourceRange::builder()
+                vk::ImageSubresourceRange::default()
                     .aspect_mask(vk::ImageAspectFlags::COLOR)
                     .base_mip_level(base_mip_level)
                     .level_count(mip_levels)
-                    .layer_count(1)
-                    .build(),
+                    .layer_count(1),
             )
             .src_access_mask(src_access_mask)
             .dst_access_mask(dst_access_mask);
@@ -385,7 +415,7 @@ impl Vulkan {
                 vk::DependencyFlags::empty(),
                 &[],
                 &[],
-                &[image_barrier.build()],
+                &[image_barrier],
             );
         }
     }
@@ -402,7 +432,7 @@ impl Vulkan {
         dst_height: u32,
         dst_mip_level: u32,
     ) {
-        let blit_info = vk::ImageBlit::builder()
+        let blit_info = vk::ImageBlit::default()
             .src_offsets([
                 vk::Offset3D { x: 0, y: 0, z: 0 },
                 vk::Offset3D {
@@ -412,11 +442,10 @@ impl Vulkan {
                 },
             ])
             .src_subresource(
-                vk::ImageSubresourceLayers::builder()
+                vk::ImageSubresourceLayers::default()
                     .aspect_mask(vk::ImageAspectFlags::COLOR)
                     .mip_level(src_mip_level)
-                    .layer_count(1)
-                    .build(),
+                    .layer_count(1),
             )
             .dst_offsets([
                 vk::Offset3D { x: 0, y: 0, z: 0 },
@@ -427,11 +456,10 @@ impl Vulkan {
                 },
             ])
             .dst_subresource(
-                vk::ImageSubresourceLayers::builder()
+                vk::ImageSubresourceLayers::default()
                     .aspect_mask(vk::ImageAspectFlags::COLOR)
                     .mip_level(dst_mip_level)
-                    .layer_count(1)
-                    .build(),
+                    .layer_count(1),
             );
 
         unsafe {
@@ -441,7 +469,7 @@ impl Vulkan {
                 vk::ImageLayout::TRANSFER_SRC_OPTIMAL,
                 *dst_image,
                 vk::ImageLayout::TRANSFER_DST_OPTIMAL,
-                &[blit_info.build()],
+                &[blit_info],
                 vk::Filter::LINEAR,
             );
         }
@@ -534,13 +562,12 @@ impl Vulkan {
             vk::PipelineStageFlags::TRANSFER,
         );
 
-        let buffer_image_copy = vk::BufferImageCopy::builder()
+        let buffer_image_copy = vk::BufferImageCopy::default()
             .image_subresource(
-                vk::ImageSubresourceLayers::builder()
+                vk::ImageSubresourceLayers::default()
                     .aspect_mask(vk::ImageAspectFlags::COLOR)
                     .mip_level(mip_level)
-                    .layer_count(1)
-                    .build(),
+                    .layer_count(1),
             )
             .image_offset(vk::Offset3D { x: 0, y: 0, z: 0 })
             .image_extent(vk::Extent3D {
@@ -555,39 +582,44 @@ impl Vulkan {
                 *image,
                 vk::ImageLayout::TRANSFER_SRC_OPTIMAL,
                 self.buffer,
-                &[buffer_image_copy.build()],
+                &[buffer_image_copy],
             );
         }
     }
 
     fn begin_commands(&self) -> Result<(), Box<dyn Error>> {
-        let command_buffer_info = vk::CommandBufferBeginInfo::builder()
+        let command_buffer_info = vk::CommandBufferBeginInfo::default()
             .flags(vk::CommandBufferUsageFlags::ONE_TIME_SUBMIT);
 
         unsafe {
             self.device
-                .begin_command_buffer(self.command_buffers[0], &command_buffer_info)?
-        };
+                .begin_command_buffer(self.command_buffers[0], &command_buffer_info)
+                .map_err(anyhow::Error::msg)?;
+        }
 
         Ok(())
     }
 
     fn submit_commands(&self) -> Result<(), Box<dyn Error>> {
         unsafe {
+            // End the command buffer
             self.device
                 .end_command_buffer(self.command_buffers[0])
-                .map_err(|e| Box::new(e) as Box<dyn Error>)?
+                .map_err(anyhow::Error::msg)?;
         };
 
-        let submit_info = vk::SubmitInfo::builder().command_buffers(&self.command_buffers);
+        let submit_info = vk::SubmitInfo::default().command_buffers(&self.command_buffers);
 
         unsafe {
+            // Submit the command buffers to the queue
             self.device
-                .queue_submit(self.queue, &[submit_info.build()], self.fence)
-                .map_err(|e| Box::new(e) as Box<dyn Error>)?;
+                .queue_submit(self.queue, &[submit_info], self.fence)
+                .map_err(anyhow::Error::msg)?;
+
+            // Wait for the fences
             self.device
                 .wait_for_fences(&[self.fence], true, FENCES_TIMEOUT_NS)
-                .map_err(|e| Box::new(e) as Box<dyn Error>)?;
+                .map_err(anyhow::Error::msg)?;
         }
 
         Ok(())
