ebb43967b8f66eea4bd1ed16d1ea7af264eba8c8
Maxim Baz <git@maximbaz.com>
2024-10-21 16:56:31 +0200

    keep 3 full pixels in the log

--- src/frame/mod.rs
+++ src/frame/mod.rs
@@ -6,7 +6,7 @@ pub mod vulkan;
 
 pub fn compute_perceived_lightness_percent(rgbas: &[u8], has_alpha: bool, pixels: usize) -> u8 {
     let mut v = rgbas.to_vec();
-    v.truncate(10);
+    v.truncate(12);
     println!("{:?}", v);
     let channels = if has_alpha { 4 } else { 3 };
 
