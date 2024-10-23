064f2532d6c11a5f13c912a998c5d18b4ca308a2
Maxim Baz <git@maximbaz.com>
2024-10-23 14:29:21 +0200

    rename capturer file to wayland, it will autodetect protocols

--- src/frame/capturer/mod.rs
+++ src/frame/capturer/mod.rs
@@ -1,7 +1,7 @@
 use crate::predictor::Controller;
 
 pub mod none;
-pub mod wlr_export_dmabuf_unstable_v1;
+pub mod wayland;
 
 pub trait Capturer {
     fn run(&mut self, output_name: &str, controller: Controller);
